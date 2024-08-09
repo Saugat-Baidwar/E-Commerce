093a78cdc42ec0b5a26fad52390dfc66d6ebe8d7
saugat-baidwar <saugat.baidwar55@gmail.com>
2024-08-09 09:55:19 +0545

    body datafetching using map from store

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,12 +1,22 @@
+import bodyData from "../Data/bodyData";
+
 function Body() {
   return (
     <>
-      <div>
-        <div className="">
-          <h1 className="text-[#FF3269] text-4xl font-bold flex justify-end mr-6">
-            Explore By Categories
-          </h1>
+      <div className="w-[1280px] mx-auto">
+        <p className="text-[#FF3269] text-4xl font-semibold">
+          Explore By Category{" "}
+        </p>
+        <div className="mt-8">
+          {bodyData.map((item) => (
+            <p>
+              <img src={item.image} alt="" />
+            </p>
+          ))}
         </div>
+        <p className="text-[#FF3269] text-2xl font-semibold flex justify-end">
+          View all Categories
+        </p>
       </div>
     </>
   );
---
+++ src/Data/bodyData.ts
@@ -0,0 +1,18 @@
+type TbodyData = {
+  image: string;
+};
+const bodyData: TbodyData[] = [
+  {
+    image: "/bodyimg1.png",
+  },
+  {
+    image: "/bodyimg2.png",
+  },
+  {
+    image: "/bodyimg3.png",
+  },
+  {
+    image: "/bodyimg4.png",
+  },
+];
+export default bodyData;
